66149a62f46833fe6d22fde00b579d580ba2266c
Daniel Edgecombe <edgy.edgemond@gmail.com>
2024-03-10 17:10:56 +0000

    fix: Bugfix python template depends, and allow pytest fixture to generate database connection based on configuration

--- src/pogo_migrate/testing.py
+++ src/pogo_migrate/testing.py
@@ -1,13 +1,23 @@
-import asyncpg
+from __future__ import annotations
 
-from pogo_migrate import config, migrate
+import os
+import typing as t
+
+from pogo_migrate import config, migrate, sql
+
+if t.TYPE_CHECKING:
+    import asyncpg
 
 
-async def apply(db: asyncpg.Connection) -> None:
+async def apply(db: asyncpg.Connection | None = None) -> None:
     c = config.load_config()
+    if db is None:
+        db = await sql.get_connection(os.environ[c.database_env_key])
     await migrate.apply(c, db)
 
 
-async def rollback(db: asyncpg.Connection) -> None:
+async def rollback(db: asyncpg.Connection | None = None) -> None:
     c = config.load_config()
+    if db is None:
+        db = await sql.get_connection(os.environ[c.database_env_key])
     await migrate.rollback(c, db)
