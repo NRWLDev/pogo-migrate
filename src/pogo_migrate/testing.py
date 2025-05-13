b090364716222cae2632c5d126e9ca6d626bad8b
Daniel Edgecombe <edgy.edgemond@gmail.com>
2025-05-13 08:51:41 +0100

    update documentation

--- src/pogo_migrate/testing.py
+++ src/pogo_migrate/testing.py
@@ -3,29 +3,26 @@ from __future__ import annotations
 import typing as t
 
 from pogo_migrate import config, migrate, sql
-from pogo_migrate.context import Context
 
 if t.TYPE_CHECKING:
     import asyncpg
 
 
 async def apply(db: asyncpg.Connection | None = None) -> None:
-    context = Context()
     c = config.load_config()
     db_ = db if db is not None else await sql.get_connection(c.database_dsn)
     try:
-        await migrate.apply(db_, c.migrations, logger=context)
+        await migrate.apply(db_, c.migrations)
     finally:
         if db is None:
             await db_.close()
 
 
 async def rollback(db: asyncpg.Connection | None = None) -> None:
-    context = Context()
     c = config.load_config()
     db_ = db if db is not None else await sql.get_connection(c.database_dsn)
     try:
-        await migrate.rollback(db_, c.migrations, logger=context)
+        await migrate.rollback(db_, c.migrations)
     finally:
         if db is None:
             await db_.close()
